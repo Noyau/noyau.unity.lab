c7f6902aa4558f529b391bcdacd07c11aaa0f30f
Romain Fontaine <noyau.dev@gmail.com>
2018-09-19 22:03:17 +0200

    Removed obsoletes
    Clearer code

--- Packages/noyau.lab.procedural/Procedural/Scripts/ColorSettings.cs
+++ Packages/noyau.lab.procedural/Procedural/Scripts/ColorSettings.cs
@@ -5,9 +5,10 @@ namespace Noyau.Lab.Procedural
     [CreateAssetMenu(menuName = "Noyau Lab/Procedural/Color Settings")]
     public sealed class ColorSettings : ScriptableObject, ISettings
     {
-        [System.Obsolete("Use \"gradient\" value instead")]
-        [ColorUsage(false, true)] public Color color = Color.white;
+        [Header("Shared Material")]
         public Material material = null;
+
+        [Header("Material Properties")]
         public GradientParam gradient = new GradientParam("_Gradient");
         public ShaderParam elevation = new ShaderParam("_Elevation");
     } // class: ColorSettings
